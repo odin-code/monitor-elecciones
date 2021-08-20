b9c615d9e51c5a9ee1df64a747dca7104115a596
imanolrtega <imanolrtega@gmail.com>
2021-08-19 21:26:11 -0300

    fetching tweets

--- components/TweetsCol/TweetsCol.js
+++ components/TweetsCol/TweetsCol.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TweetEmbed from "./TweetEmbed";
+import * as data from "../../constants/tweets.json";
 
 const TweetsCol = () => {
+  const [dataTweets, setDataTweets] = useState(data);
+  const [tweets, setTweets] = useState(data.includes.tweets);
+  const [users, setUsers] = useState(data.includes.users);
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetch(
+        "https://argentina-2021.herokuapp.com/tweets?ht=carpincho"
+      ).then((response) =>
+        response.json().then((data) => {
+          setDataTweets(data);
+          setTweets(data.includes.tweets);
+          setUsers(data.includes.users);
+        })
+      );
+    };
+    getData();
+    //console.log(dataTweets);
+    //console.log(tweets);
+    //console.log(users);
+  }, []);
+
   return (
     <div className="w-3/12 bg-gray-200 dark:bg-gray-850 flex flex-col p-6 no-scrollbar overflow-auto">
       <div className="mt-3">
@@ -13,10 +36,9 @@ const TweetsCol = () => {
         </h4>
       </div>
       <div className="flex flex-col space-y-6">
-        <TweetEmbed />
-        <TweetEmbed />
-        <TweetEmbed />
-        <TweetEmbed />
+        {tweets?.map((tweet) => (
+          <TweetEmbed key={tweet.id} tweet={tweet} users={users} />
+        ))}
       </div>
     </div>
   );
