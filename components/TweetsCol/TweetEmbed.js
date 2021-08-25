581d551040e20e0f4af23469ad1824d39966dd05
imanolrtega <imanolrtega@gmail.com>
2021-08-25 10:03:17 -0300

    fix: add the quotes and retweets for the metrics of each tweet

--- components/TweetsCol/TweetEmbed.js
+++ components/TweetsCol/TweetEmbed.js
@@ -97,7 +97,10 @@ const TweetEmbed = ({ tweet, users, medias, quotedTweets }) => {
   }, [repliedTo]);
 
   useEffect(() => {
-    users && repliedToData && repliedToData.author_id && getUserReplied(repliedToData.author_id);
+    users &&
+      repliedToData &&
+      repliedToData.author_id &&
+      getUserReplied(repliedToData.author_id);
   }, [repliedToData]);
 
   return (
@@ -200,11 +203,7 @@ const TweetEmbed = ({ tweet, users, medias, quotedTweets }) => {
                   alt={formattedText}
                   height={m.height}
                   width={m.width}
-                  src={
-                    m.url
-                      ? m.url
-                      : m.preview_image_url
-                  }
+                  src={m.url ? m.url : m.preview_image_url}
                   className="rounded"
                 />
               ))}
@@ -251,7 +250,10 @@ const TweetEmbed = ({ tweet, users, medias, quotedTweets }) => {
                   d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"
                 />
               </svg>
-              <span>{tweet.public_metrics.retweet_count}</span>
+              <span>
+                {tweet.public_metrics.retweet_count +
+                  tweet.public_metrics.quote_count}
+              </span>
             </a>
             <a
               title="Dar like"
