f2df158c63d27099c9754228dc1b049f89d6ecb6
Imanol Ortega <imanolrtega@gmail.com>
2021-11-09 13:44:24 -0300

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -7,19 +7,19 @@ export const channels = [
   },
   {
     name: "La Nación +",
-    embedId: "5ql5hNZQzKc",
+    embedId: "6ilZeMFyNE8",
     img: "https://i.ibb.co/ZWVGgkH/la-nacion.jpg",
     key: 1,
   },
   {
     name: "C5N",
-    embedId: "1xKycSal5SE",
+    embedId: "RmvgK_GWQo0",
     img: "https://i.ibb.co/sjxWwvz/c5n.jpg",
     key: 2,
   },
   {
     name: "Crónica TV",
-    embedId: "GyRdMkZcFWk",
+    embedId: "RWnXuePjXAU",
     img: "https://i.ibb.co/9Tt6Ftr/cronica-tv.jpg",
     key: 3,
   },
